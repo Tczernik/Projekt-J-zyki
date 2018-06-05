d45116f471d18bda8be246f4fabd1dc67b328fb5
Tczernik <tomasz.czernik1997@gmail.com>
2018-06-05 18:06:07 +0200

    Database/models

---
+++ DoctorManagement/DoctorManagement/Controllers/EnrollmentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoctorManagement.Models;
+
+namespace DoctorManagement.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private DoctorDataEntities db = new DoctorDataEntities();
+
+        // GET: Enrollments
+        public ActionResult Index()
+        {
+            var enrollment = db.Enrollment.Include(e => e.Disease).Include(e => e.Patient);
+            return View(enrollment.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollment.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.DiseaseID = new SelectList(db.Disease, "DiseaseID", "Title");
+            ViewBag.PatientID = new SelectList(db.Patient, "PatientID", "LastName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentID,Grade,DiseaseID,PatientID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollment.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DiseaseID = new SelectList(db.Disease, "DiseaseID", "Title", enrollment.DiseaseID);
+            ViewBag.PatientID = new SelectList(db.Patient, "PatientID", "LastName", enrollment.PatientID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollment.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DiseaseID = new SelectList(db.Disease, "DiseaseID", "Title", enrollment.DiseaseID);
+            ViewBag.PatientID = new SelectList(db.Patient, "PatientID", "LastName", enrollment.PatientID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentID,Grade,DiseaseID,PatientID")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DiseaseID = new SelectList(db.Disease, "DiseaseID", "Title", enrollment.DiseaseID);
+            ViewBag.PatientID = new SelectList(db.Patient, "PatientID", "LastName", enrollment.PatientID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollment.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollment.Find(id);
+            db.Enrollment.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DoctorManagement/DoctorManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoctorManagement.MvcApplication" Language="C#" %>
